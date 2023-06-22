6d466cfb2633e6c174708e7f6ce675f878fd2014
ShaymaTechini <chaima.techini@esprit.tn>
2023-06-22 18:51:53 +0100

    stock management modification

--- Suppliers/src/main/java/com/esprit/stockManagement/SuppliersApplication.java
+++ Suppliers/src/main/java/com/esprit/stockManagement/SuppliersApplication.java
@@ -1,15 +1,50 @@
 package com.esprit.stockManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import entities.Supplier;
+import repositories.SupplierRepository;
+import services.IsupplierService;
+import services.SupplierService;
+
+
+
+
+
+
+
 
 @EnableEurekaClient
 @SpringBootApplication
+@ComponentScan(basePackages = "com.esprit.stockManagement")
 public class SuppliersApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuppliersApplication.class, args);
+		System.out.print("*********");
 	}
+	
+	
+	@Bean
+	ApplicationRunner start(SupplierRepository repo ) {
+		return (args) -> {
+			System.out.print("888888888888888888888888888888888");
+			// save
+			repo.save(new Supplier("Supplier1",  "tunis","Supplier1@gmail.com"));
+			repo.save(new Supplier("Supplier2", "sousse","Supplier2@gmail.com"));
+
+			// fetch
+			repo.findAll().forEach(System.out::println);
+
+		};
+	}
+
 
 }
