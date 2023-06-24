ecbeefb43a16d8f6b872223e3dcab03b2e2c92e1
omria <omria@DESKTOP-S5A4AL5>
2023-06-24 13:27:27 +0100

    NodeJS

--- nodemicro/app.js
+++ nodemicro/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-//const eurekaHelper = require('./eureka-helper');
+const eurekaHelper = require('./eureka-helper');
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const mongoose = require("mongoose")
